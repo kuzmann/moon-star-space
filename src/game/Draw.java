e73f1ab2844900da703679c8eb548b0a588d139a
Kuzmann <s60129@beuth-hochschule.de>
2018-11-17 15:39:40 +0100

    add function for drawing with JLabel

--- src/game/Draw.java
+++ src/game/Draw.java
@@ -1,2 +1,19 @@
-public class Draw {
+import javax.swing.*;
+import java.awt.*;
+
+public class Draw extends JLabel{
+
+    private static final long serialVersionUID = 1L;
+
+
+    protected void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+    }
+
 }
