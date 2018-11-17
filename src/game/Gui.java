3a211bd1856d8069218e66ade65fb47102f3cae5
Kuzmann <s60129@beuth-hochschule.de>
2018-11-17 17:04:33 +0100

    set a resetButton to start a new game

--- src/game/Gui.java
+++ src/game/Gui.java
@@ -1,6 +1,9 @@
 package game;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * This class provides the graphical user interface and interaction methods.
@@ -12,6 +15,8 @@ public class Gui {
     JFrame gameframe;
     //Objekt der Klasse Draw
     Draw draw;
+    //ResetButton to start a new game
+    JButton resetButton;
 
     //Button für die Bedienung (9 Stück)
     static JButton button[] = new JButton[9];
@@ -38,12 +43,28 @@ public class Gui {
             button[i] = new JButton();
             button[i].setVisible(true);
             button[i].addActionListener(new Controller());
+            //Text nicht umrandet
             button[i].setFocusPainted(false);
+            //Inhalt der Button sind nicht sichtbar
             button[i].setContentAreaFilled(false);
+            //Ränder der Button sind nicht sichtbar
             button[i].setBorder(null);
             gameframe.add(button[i]);
         }
 
+        //Start new game Button
+        resetButton = new JButton("Reset");
+        resetButton.setBounds(675, 500, 100, 40);
+        resetButton.setVisible(true);
+        resetButton.setBackground(new Color(51, 102, 153));
+        resetButton.setForeground(Color.WHITE);
+        resetButton.setFocusPainted(false);
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Restart.reset();
+            }
+        });
 
 
 
