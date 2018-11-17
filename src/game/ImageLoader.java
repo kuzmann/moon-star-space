10ab13f40458a2d7cf26a55e326546d35402fec6
Kuzmann <s60129@beuth-hochschule.de>
2018-11-17 16:59:15 +0100

    set png files

--- src/game/ImageLoader.java
+++ src/game/ImageLoader.java
@@ -1,5 +1,22 @@
 package game;
 
 
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
 public class ImageLoader {
+
+    static Image moon, star;
+
+    public ImageLoader() {
+        try {
+            moon = ImageIO.read(new File("res/moon.png"));
+            star = ImageIO.read(new File("res/star.png"));
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
