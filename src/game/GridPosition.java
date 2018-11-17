61e9fee1c9302a9853174607cef8b05ff6334c15
Kuzmann <s60129@beuth-hochschule.de>
2018-11-17 16:19:51 +0100

    add new class GridPosition

---
+++ src/game/GridPosition.java
@@ -0,0 +1,24 @@
+package game;
+
+public class GridPosition {
+
+    public static void position(){
+
+        //Row 1
+        Gui.button[0].setBounds(175, 50, 150, 150);
+        Gui.button[1].setBounds(325, 50, 150, 150);
+        Gui.button[2].setBounds(475, 50, 150, 150);
+
+        //Row 2
+        Gui.button[3].setBounds(175, 200, 150, 150);
+        Gui.button[4].setBounds(325, 200, 150, 150);
+        Gui.button[5].setBounds(475, 200, 150, 150);
+
+        //Row 3
+        Gui.button[6].setBounds(175, 350, 150, 150);
+        Gui.button[7].setBounds(325, 350, 150, 150);
+        Gui.button[8].setBounds(475, 350, 150, 150);
+
+    }
+
+}
